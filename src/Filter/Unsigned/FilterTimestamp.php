25053c98b77de8b1f99e4ae7be3112b9f7805f91
xcg <bccai2010@qq.com>
2019-10-17 11:50:22 +0800

    fix

---
+++ src/Filter/Unsigned/FilterTimestamp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xcg
+ * Date: 2019/10/16
+ * Time: 14:49
+ */
+
+namespace EasySwoole\DDL\Filter\Unsigned;
+
+
+use EasySwoole\DDL\Blueprint\Column;
+use EasySwoole\DDL\Contracts\FilterInterface;
+
+class FilterTimestamp implements FilterInterface
+{
+
+    public static function run(Column $column)
+    {
+        if ($column->getUnsigned()) {
+            throw new \InvalidArgumentException('col ' . $column->getColumnName() . ' type timestamp no require unsigned ');
+        }
+    }
+}
